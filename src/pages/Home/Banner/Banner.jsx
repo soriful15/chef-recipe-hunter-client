10c9e2c912eb0e97ca69eca7af55ae8a3f0611d7
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-02 23:00:27 +0600

    baner complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import banner from '../../../assets/banner/Free-Food-Advertising-Banner-Template.jpg'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='grid lg:grid-cols-2 px-8 gap-4 mt-10'>
+
+<div>
+    <h1 className='text-5xl text-indigo-900 font-bold'>
+Enjoy food all over the world
+
+    </h1>
+    <p className='font-bold text-xl text-amber-700 mt-4'>
+    A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a foodservice operation. A recipe may be simple to complex based on the requirements of the operation and the intended user. For example, an experienced chef may need a recipe with only a few details, while a beginner cook may need more information about ingredients, preparation steps, cooking times and temperatures, visual cues, and equipment requirements. 
+    </p>
+    <button className="btn btn-active btn-primary mt-4">Get Started</button>
+</div>
+
+
+            <LazyLoadImage src={banner} alt="" className='' />
+        
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import SpecialFood from '../SpecialFood/SpecialFood';
 import BakedRecipes from '../BakedRecipes/BakedRecipes';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+
+            <section>
+
+              <Banner></Banner>
+            </section>
             <section className='1'>
                 <h1 className='text-5xl font-bold text-cyan-700 text-center mt-10'>Special Food</h1>
                 <SpecialFood></SpecialFood>
             </section>
 
             <section className='2'>
-            <h1 className='text-5xl font-bold text-cyan-700 text-center mt-16'>  Baked Recipes</h1>
-           <BakedRecipes></BakedRecipes>
+                <h1 className='text-5xl font-bold text-cyan-700 text-center mt-16'>  Baked Recipes</h1>
+                <BakedRecipes></BakedRecipes>
             </section>
         </div>
     );
