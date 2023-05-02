ca96a1ed0712bf8e4aac57f3190bc6fb9d2c0725
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-02 16:55:16 +0600

    footer is done

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../pages/Home/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-250px)]'>
+          <Outlet></Outlet>
+          </div>
+            <Footer></Footer>
         </div>
     );
 };
