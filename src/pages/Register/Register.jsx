e5a973e9f18034d1d7eab7860077a4febf0357c5
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-03 15:13:27 +0600

    register form html degisn done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
-
+import register from '../../assets/38435-register.json'
+import Lottie from 'lottie-react'
+import { Link } from 'react-router-dom';
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>fgxvngffgcjgfjn</h1>
-        </div>
+        <>
+            <div onSubmit={handleRegister} className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Register now!</h1>
+                        <div className=''>
+                            <div className='w-96'>
+                                <Lottie animationData={register} loop={true} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
+                            </div>
+
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text" name="photo" placeholder="Enter your Photo url" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                            <p className='text-center'>AlReady have an Account? <Link to='/login'><span className='text-green-500'>login</span></Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
